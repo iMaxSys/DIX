1f46d8d91933a667e8c3c8bff0613ac2b793bf93
Michael <12612730@qq.com>
2020-04-20 17:10:58 +0800

    new

---
+++ Dix/Controllers/IndexController.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Dix.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Dix.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class IndexController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly IServiceProvider _provider;
+        private readonly ILogger<IndexController> _logger;
+        private readonly IScope _scope;
+        private readonly IAllScope _allScope;
+
+        private readonly Func<int, IScope> _xfactory;
+        private readonly IService[] _services;
+
+
+
+        public IndexController(IServiceProvider provider, ILogger<IndexController> logger, IScope scope, IAllScope allScope, Func<int, IScope> xfactory, IServiceFactory serviceFactory)
+        {
+            _provider = provider;
+            _logger = logger;
+            _scope = scope;
+            _allScope = allScope;
+            _xfactory = xfactory;
+            _services = serviceFactory.GetService(new int[] { 3, 4 });
+            var s = xfactory(3);
+        }
+
+        [HttpGet]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            var s = _provider.GetService<IScope>();
+
+            _logger.LogInformation(_scope.Say());
+            _logger.LogInformation(s.Say());
+            _logger.LogInformation(_allScope.Say());
+
+            var xs = _xfactory(4);
+
+            _logger.LogInformation(xs.Say());
+
+            foreach (var item in _services)
+            {
+                _logger.LogInformation(item.Say());
+            }
+
+            //_logger.LogWarning("it's warning.");
+            //_logger.LogError("it's trace.");
+            var rng = new Random();
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = rng.Next(-20, 55),
+                Summary = Summaries[rng.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+    }
+}
---
+++ Dix/Startup.cs
@@ -0,0 +1,84 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Dix.Services;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace Dix
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+
+            services.AddSingleton<ISingleton, Singleton>();
+            services.AddScoped<IScope, Scope>();
+            services.AddScoped<IScope, Scope1>();
+            services.AddScoped<IScope, Scope2>();
+            services.AddScoped<IScope, Scope3>();
+            services.AddScoped<IScope, Scope4>();
+            services.AddScoped<IServiceFactory, ServiceFactory>();
+            services.AddScoped<IAllScope, AllScope>();
+            services.AddTransient<ITransit, Transit>();
+            services.AddTransient<ITransit, Transit1>();
+
+            services.AddScoped<Scope3>();
+            services.AddScoped<Scope4>();
+
+            services.AddScoped(provider =>
+            {
+                Func<int, IScope> func = n =>
+                {
+                    switch (n)
+                    {
+                        case 3:
+                            return provider.GetService<Scope3>();
+                        case 4:
+                            return provider.GetService<Scope4>();
+                        default:
+                            throw new NotSupportedException();
+                    }
+                };
+                return func;
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
